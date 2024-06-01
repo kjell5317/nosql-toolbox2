a7c61042ae0779c494ac61c6e5f89a05825d7342
Kjell Hanken <69535642+kjell5317@users.noreply.github.com>
2024-06-01 19:45:48 +0200

    assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,4 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [react(), tailwind()],
   site: "https://kjell5317.github.io",
-  base: "/nosql-toolbox2",
 });
