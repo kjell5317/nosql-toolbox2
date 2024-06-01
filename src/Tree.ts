dfec7a6273f0095f3a895d9eaf3ba0ba49cd1c72
Kjell Hanken <69535642+kjell5317@users.noreply.github.com>
2024-06-01 19:34:39 +0200

    prototype

---
+++ src/Tree.ts
@@ -0,0 +1,43 @@
+export class Tree {
+    static root: Tree;
+    name: string;
+    answer: string | null;
+    children: Tree[];
+
+    constructor(json: any, root: boolean = false) {
+        if (root) {
+            Tree.root = this;
+        }
+        this.name = json.name;
+        this.answer = json.answer;
+        this.children = json.children.map((child: any) => new Tree(child));
+    }
+
+    next(n: number): Tree | null {
+        if (this.children.length <= n) {
+            return null;
+        }
+        return this.children[n];
+    }
+
+    static findParent(node: Tree, target: Tree): Tree | null {
+        for (let child of node.children) {
+            if (child == target) {
+                return node;
+            } else {
+                const parent = Tree.findParent(child, target);
+                if (parent) {
+                    return parent;
+                }
+            }
+        }
+        return null;
+    }
+
+    back(): Tree | null {
+        if (this == Tree.root) {
+            return null; // Root node has no parent
+        }
+        return Tree.findParent(Tree.root, this);
+    }
+}
---
+++ src/components/Question.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { Tree } from "../Tree";
+
+export default function Question({ init }: { init: any }) {
+
+    const [node, setNode] = useState(() => new Tree(init, true));
+
+    const next = (n: number) => (e: React.MouseEvent<HTMLButtonElement>) => {
+        const child = node.next(n);
+        if (child) {
+            setNode(child);
+        }
+    }
+
+    const back = () => {
+        const parent = node.back();
+        if (parent) {
+            setNode(parent);
+        }
+    }
+
+    const root = () => {
+        setNode(Tree.root);
+    }
+
+    return (
+        <>
+            <div className="flex">
+                <div className="flex flex-1 gap-4">
+                    {Tree.root != node && <>
+                        <button onClick={back} className="btn btn-circle btn-outline btn-sm"><img src="/public/Arrow_alt_left.svg" alt="Back button" className="pl-0.5"/></button>
+                        <button onClick={root} className="btn btn-circle btn-outline btn-sm"><img src="/public/Refresh.svg" alt="Reset button" className="pb-1" /></button>
+                    </>}
+                </div>
+                <button className="btn btn-circle btn-outline btn-sm"><img src="/public/Info.svg" alt="Info button" className="pb-0.5" /></button>
+            </div>
+            <h1 className="text-2xl mb-4 text-center">{node.name}</h1>
+            <div className="flex flex-col gap-4 items-center">
+                {node.children.map((child, i) => (
+                    <button key={i} className="btn btn-primary w-2/3" onClick={next(i)}>{child.answer}</button>
+                ))}
+            </div>
+        </>
+    );
+}
